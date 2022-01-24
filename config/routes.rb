3fc08b62f9e94000d3fe054d93e9c2547d69fb94
nishinotakaya <takaya314boxing@gmail.com>
2022-01-24 23:04:14 +0900

    fix/bootstrap追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     confirmations: 'users/confirmations'
 }
+
+devise_scope :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+  get '/users/dash_bord'
+end
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
