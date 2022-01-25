cbecf9b9f598c9fa87d5e85440417d6254b4b874
nishinotakaya <takaya314boxing@gmail.com>
2022-01-26 00:12:27 +0900

    fix/developのpasswordお忘れ時時メールアドレス認証完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,19 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
-    confirmations: 'users/confirmations'
+    confirmations: 'users/confirmations',
 }
+
+devise_scope :user do
+  root :to => "users/sessions#new"
+  put 'confirmation', to: 'users/confirmations#show', as: :back_confirmation
+end
 
 devise_scope :user do
   get '/users/sign_out' => 'devise/sessions#destroy'
   get '/users/dash_bord'
+  get '/users/login_before'
 end
-  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
