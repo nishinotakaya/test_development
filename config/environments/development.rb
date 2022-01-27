37cb79e86a24111ffcedc725da708305d5b19e92
nishinotakaya <takaya314boxing@gmail.com>
2022-01-27 21:23:01 +0900

    development.rb変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV['GOOGLE_MAIL_ADDRESS'], #gmailアドレス
+    :password => ENV['GOOGLE_MAIL_PASSWORD'], #gmailパスワード
+    :authentication => 'login',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.perform_deliveries = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -27,12 +40,11 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
