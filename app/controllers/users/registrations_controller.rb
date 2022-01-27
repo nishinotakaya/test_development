a96f4f4993dd71b1c1ade0c6f72275395ea1c139
nishinotakaya <takaya314boxing@gmail.com>
2022-01-27 13:01:25 +0900

    fix/日本語化

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -62,6 +62,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(resource)
-    users_login_before_path(resource)
+    new_user_session_path
   end
 end
